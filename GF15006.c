4ea6e176f2ff3f4e0f6cb8af00b6958c94f94815
kevin-Oz <31225283+kevin-Oz@users.noreply.github.com>
2017-10-18 16:21:31 -0600

    Add files via upload

---
+++ GF15006.c
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<limits.h>
+
+struct Stack{
+int top;
+unsigned capacidad;
+int* arreglo;
+};
+
+struct Stack* createStack(unsigned capacidad)
+{
+	struct Stack* pila=(struct Stack*)malloc (sizeof(struct Stack));
+
+	pila->top=-1;
+	pila->arreglo=(int*)malloc(pila->capacidad * sizeof(int));
+	 return pila;
+	
+}
+
+void push(struct Stack* pila ,int valor){
+
+	pila->arreglo[++pila->top]=valor;
+	printf("valor pusheado  %d\n",valor);
+}
+
+int pop(struct Stack* pila){
+	return pila-> arreglo[pila->top--];
+}
+int peek(struct Stack* pila){
+
+return pila-> arreglo[pila->top];
+}
+
+int main(int argc, char const *argv[])
+{
+	int n,num,con=0;
+	
+	printf("Cuantos numeros ");
+	scanf("%d ", &n);
+
+      int vec[n];
+
+	for(int i=0;i<n;i++){
+	 printf("Ingrese el numero en la posicion %d", i+1);
+	 scanf("%d",&num);
+	 
+      vec[i]=num;
+    
+      	
+
+	}
+	for (int i = 0; i < (n-1); ++i)
+	{
+			if(vec[i]!=vec[i+1]){
+         con++;
+      }
+	}
+	
+struct Stack* pila=createStack(10);
+struct Stack* pila1=createStack(n);
+
+for (int i = 0; i <n; ++i)
+{
+	push(pila, vec[i]);
+}
+
+
+
+int fuera =0, ultimo=0;
+fuera=pop(pila);
+ultimo=peek(pila);
+
+printf("Elemento sacado %d ",fuera);
+printf("Ultimo elemento  %d",ultimo);
+	return 0;
+}
+
+
+
+
